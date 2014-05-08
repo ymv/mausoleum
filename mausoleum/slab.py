d7b20da1b859ce0221d741ded20a5159a74e59d8
Yourkov Mikhail <yourkovm@gmail.com>
2014-05-09 02:12:27 +0400

    Compress segments

--- mausoleum/slab.py
+++ mausoleum/slab.py
@@ -2,6 +2,7 @@ from struct import pack, unpack
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
+import zlib
 
 def random_string(l):
     with open('/dev/urandom') as f:
@@ -18,15 +19,21 @@ class SlabFile(object):
     def close(self):
         return self._file.close()
 
-    def write(self, hash, payload):
+    def write(self, hash, payload, compress=True):
         key = random_string(16)
         iv = random_string(16)
         encrypted_key = PKCS1_OAEP.new(self._pk).encrypt(key+iv)
-
+        
+        format = 1
+        if compress:
+            payload_compressed = zlib.compress(payload)
+            if len(payload) - len(payload_compressed) > 16:
+                format = 2
+                payload = payload_compressed
         pad = 16 - len(payload) % 16 
         data = payload + chr(pad)*pad
         encrypted_payload = AES.new(key, mode=AES.MODE_CBC, IV=iv).encrypt(data)
-        header = pack('!b16sL', 1, hash.digest(), len(encrypted_key))
+        header = pack('!b16sL', format, hash.digest(), len(encrypted_key))
         self._file.write(pack('!bL', 1, len(encrypted_payload)+len(encrypted_key)+len(header)))
         self._file.write(header)
         self._file.write(encrypted_key)
@@ -44,7 +51,7 @@ class SlabFile(object):
         segment_header = self._file.read(21)
         segment_encoding, hash, key_size = unpack('!b16sL', segment_header)
 
-        if segment_encoding != 1:
+        if segment_encoding != 1 and segment_encoding != 2:
             raise Exceptin('Bad segment encoding: %s' % segment_encoding)
 
         encrypted_key_iv = self._file.read(key_size)
@@ -54,6 +61,9 @@ class SlabFile(object):
         encrypted_payload = self._file.read(chunk_length - key_size - 21)
         padded = AES.new(key, mode=AES.MODE_CBC, IV=iv).decrypt(encrypted_payload)
         payload = padded[:-ord(padded[-1])]
+
+        if segment_encoding == 2:
+            payload = zlib.decompress(payload)
 
         return hash, payload, segment_encoding
 
